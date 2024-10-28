9a4f2bfcea96128f7c816abbc9a7c6c2a81bd58e
Mel-lyssa <u666673582@byui.edu>
2024-10-27 22:34:35 -0600

    footer done

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,25 @@
 'use client';
 
+import Link from 'next/link';
 
 export function Footer() {
     return (
-        <footer className={'p-6'}>
-            footer
+        <footer className='bg-white dark:bg-black py-4 border-t border-gray-200 dark:border-gray-800'>
+            <div className="max-w-[1600px] mx-auto px-4 sm:px-8 lg:px-[144px]">
+                <div className="bg-white dark:bg-black bg-opacity-90 flex flex-col items-center p-4 w-full max-w-[1600px] space-y-4">
+                    {/* Links Section */}
+                    <div className="flex flex-wrap justify-center space-x-8">
+                        <Link href="/" className="text-black dark:text-white text-[16px] font-[400] leading-[28px] cursor-pointer hover:text-teal-500 dark:hover:text-teal-300">Home</Link>
+                        <Link href="/about" className="text-black dark:text-white text-[16px] font-[400] leading-[28px] cursor-pointer hover:text-teal-500 dark:hover:text-teal-300">About</Link>
+                        <Link href="/projects" className="text-black dark:text-white text-[16px] font-[400] leading-[28px] cursor-pointer hover:text-teal-500 dark:hover:text-teal-300">Projects</Link>
+                        <Link href="/uses" className="text-black dark:text-white text-[16px] font-[400] leading-[28px] cursor-pointer hover:text-teal-500 dark:hover:text-teal-300">Uses</Link>
+                    </div>
+                    {/* Copyright Section */}
+                    <div className="text-zinc-400 dark:text-zinc-500 text-[14px] font-[400] leading-[24px] text-center">
+                        © 2024 John Doe. All rights reserved.
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
